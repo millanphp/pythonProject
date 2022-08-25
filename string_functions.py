de7123ec69a819602821234f4fb7b2de2a7469a1
millanphp <millanphp@gmail.com>
2022-08-25 20:25:22 +1000

    checkin

---
+++ string_functions.py
@@ -0,0 +1,17 @@
+#Entering the first string
+print("enter a string: ")
+a= input()
+print(f"Your string value < {a} >" )
+
+#Applying method to class
+print(f"Applying method lower() on string < {a} >" )
+
+#Applying lower case
+print(f"Result is, {a.lower()}")
+
+#Applying upper case
+print(f"Result is, {a.upper()}")
+
+#Applying swapcase
+print(f"Result is, {a.swapcase()}")
+
